bf62bea0b2038db5bc34b12395da4d3a8b4f145a
EricChukoltyansky <kazohelmet@gmail.com>
2023-02-12 15:48:01 +0200

    fixed import

--- youtubeScraper.js
+++ youtubeScraper.js
@@ -1,6 +1,6 @@
 // import puppeteer from "puppeteer";
 import puppeteer from "puppeteer-extra";
-import StealthPlugin from ("puppeteer-extra-plugin-stealth");
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 puppeteer.use(StealthPlugin());
 
